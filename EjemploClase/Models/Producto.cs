67e7051a2ab25d87a25012d3886ab14371eef6e6
yanet <yanet@YANET>
2024-08-11 03:18:19 -0300

    Agregar archivos de proyecto.

---
+++ EjemploClase/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjemploClase.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public decimal Precio { get; set; }
+
+    public int Cantidad { get; set; }
+}
---
+++ EjemploClase/Models/ProductoAbmContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjemploClase.Models;
+
+public partial class ProductoAbmContext : DbContext
+{
+    public ProductoAbmContext()
+    {
+    }
+
+    public ProductoAbmContext(DbContextOptions<ProductoAbmContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=YANET\\SQLEXPRESS; DataBase=ProductoABM; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC073DA0ABD5");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
